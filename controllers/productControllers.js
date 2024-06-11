932cf7452a3a3919ac6ed1f70a9fbae82fc4155c
WangariNelly <nellywangari30@gmail.com>
2024-06-11 18:37:02 +0300

    Routes

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -6,6 +6,33 @@ exports.newProduct = async (req, res, next) => {
     res.status(201).json({ message: `success`, product});
 }
 
-exports.getProducts = (req, res) => {
-    res.status(200).json({ message: `success`});
+exports.getProducts = async(req, res, next) => {
+    const products = await Product.find();
+    res.status(200).json({ message: `success`, products});
+}
+
+//single product
+exports.getProduct = async(req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(404).json({ message: `Product not found`});
+    }
+        res.status(200).json({ message: `success`, product});
+}
+
+//update
+
+exports.updateProduct = async(req, res, next) => {
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(404).json({ message: `Product not found`});
+    }     
+product = await Product.findByIdAndUpdate(req.params.id, req.body,{
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+});
+res.status(200).json({ message: `success`, product});
 }
--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts, newProduct } = require('../controllers/productControllers');
+const { getProducts, newProduct, getProduct, updateProduct } = require('../controllers/productControllers');
 router.route('/products').get(getProducts);
-router.route('/product/new').post(newProduct);
+router.route('/product/:id').get(getProduct);
+router.route('/admin/product/new').post(newProduct);
+router.route('/admin/product/:id').put(updateProduct);
 
-module.exports = router;
+
+module.exports = router; 
