505965b0eee163a745f0167af4dda38077eabdb4
WangariNelly <nellywangari30@gmail.com>
2024-06-24 03:53:42 +0300

    Error Handler class

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,6 +1,6 @@
 // global error class
 class ErrorHandler extends Error {
-    constructor(message, errorCode ){
+    constructor(message, statusCode ){
         super(message);
         this.statusCodeCode = statusCode
 
