4c56489b38550bcb4701f66f17c08d25106b871e
WangariNelly <nellywangari30@gmail.com>
2024-06-27 18:01:57 +0300

    Login User and assign token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,12 @@
 const User = require('../models/user');
 const errorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const ErrorHandler = require('../utils/errorHandler');
 
 
 //Register user => api/v1/register
 
-exports.registerUser = catchAsyncErrors( async (req,res,next) => {
+exports.registerUser = catchAsyncErrors( async(req,res,next) => {
     const { name, email, password } = req.body;
 
     const user = await User.create ({
@@ -18,6 +19,33 @@ exports.registerUser = catchAsyncErrors( async (req,res,next) => {
         }
     })
 
+    const token = user.getJwtToken();
+    res.status(201).json({
+        success: true,
+        token
+    })
+})
+
+//login user /api/v1/login
+exports.loginUser = catchAsyncErrors( async( req,res,next) => {
+    const { email, password } = req.body
+
+    //check if email and pasword is entered by user 
+    if (!email || !password){
+        return next(new ErrorHandler('Please enter valid email $ password', 400));
+    }
+    //finding user in DB
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        return next(new ErrorHandler('Invalid email or password', 401));
+    }
+     
+    //checks if password is correct
+    const isPasswordMatched = await user.comparePassword(password);
+    if (!isPasswordMatched){
+        return next(new ErrorHandler('Invalid email or password', 401));
+    }
     const token = user.getJwtToken();
     res.status(201).json({
         success: true,
