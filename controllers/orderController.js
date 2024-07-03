0b5e5bf9ea81ebd2a6a38bca81dd33ae3c5a7cf7
WangariNelly <nellywangari30@gmail.com>
2024-07-03 06:23:36 +0300

    Delete orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -113,3 +113,18 @@ async function updateStock(id, quantity){
   product.stock = product.stock - quantity;
   await product.save({ validateBeforeSave: false });
 }
+
+
+//Delete order => /api/v1/admin/order/:id
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id)
+  
+  if (!order) {
+    return next(new ErrorHandler("No order Found with this ID", 404));
+  }
+
+  await order.remove();
+  res.status(200).json({
+    success: true,
+  });
+});
