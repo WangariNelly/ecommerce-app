a1fe2a948a80bce24ad65c8a4245de260e4469ad
WangariNelly <nellywangari30@gmail.com>
2024-07-03 06:10:09 +0300

    Update order quantity and stock

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,5 @@
 const Order = require("../models/order");
-const Products = require("../models/products");
+const Product = require("../models/products");
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors') 
 const ErrorHandler = require("../utils/errorHandler");
 const order = require("../models/order");
@@ -64,17 +64,52 @@ exports.myOrders = catchAsyncErrors(async (req, res, next) => {
 
 //Get all orders by admin => /api/v1/admin/orders/
 exports.allOrders = catchAsyncErrors(async (req, res, next) => {
-    const orders = await Order.find({ user: req.user.id });
+    const order = await Order.find({ user: req.user.id });
 
     let totalAmount = 0;
-    orders.forEach(order => {
+    order.forEach(order => {
       totalAmount += order.totalPrice
     });
   
     res.status(200).json({
       success: true,
       totalAmount,
-      orders,
+      order,
     })
   });
   
+
+  //Update/process  orders by admin => /api/v1/admin/orders/:id
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order !== null) {
+    console.log(order.orderStatus);
+} else {
+    console.log("Order is null or missing orderStatus property");
+}
+
+  if(order.orderStatus === 'Delivered'){
+   return next (new ErrorHandler('Order Delivered already',400));
+  }
+
+  order.orderItems.forEach( async item => {
+    await updateStock(item.product, item.quantity);
+  })
+
+  order.orderStatus = req.body.status,
+  order.deliveredAt = Date.now()
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+  })
+});
+
+async function updateStock(id, quantity){
+  const product = await Product.findById(id);
+
+  product.stock = product.stock - quantity;
+  await product.save({ validateBeforeSave: false });
+}
